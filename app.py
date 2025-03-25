4baf82f028fa06ddec9bfcf8869d7ed0c8a9733e
rooseveltalej <roperez@estudiantec.cr>
2025-03-25 13:17:36 -0600

    feat: add CORS middleware to allow cross-origin requests

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from sqlalchemy import inspect
 from database import engine, Base
@@ -8,6 +9,14 @@ from routes import router as api_router
 app = FastAPI()
 
 app.include_router(api_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todos los orígenes (es inseguro en producción)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Verificar si la base de datos ya tiene tablas
 inspector = inspect(engine)
