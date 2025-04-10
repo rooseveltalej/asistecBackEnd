84858581255ce3c8f5900f12b55cf075b26f8baa
AnthonyAriass <anthonyarias2000@estudiantec.cr>
2025-04-09 18:44:46 -0600

    POSTS / Get Recent Post - pending testing

--- controllers/posts_controllers.py
+++ controllers/posts_controllers.py
@@ -1,6 +1,7 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
+from sqlalchemy import desc
 import models
 import schemas
 from database import get_db
@@ -44,3 +45,33 @@ def create_post(post: schemas.PostBase, user_id: int, db: Session):
         content={"msg": "SUCCESS", "post_id": new_post.post_id},
         status_code=status.HTTP_201_CREATED
     )
+
+
+def get_recent_user_posts(user_id: int, db: Session = Depends(get_db)):
+    # Obtener lista de IDs de canales a los que el usuario está suscrito
+    subscribed_channel_ids = (
+        db.query(models.Subscription.channel_id)
+        .filter(models.Subscription.user_id == user_id)
+        .subquery()
+    )
+
+    # Obtener los 3 posts más recientes de esos canales
+    recent_posts = (
+        db.query(models.Post.post_id, models.Channel.channel_name)
+        .join(models.Channel, models.Post.channel_id == models.Channel.channel_id)
+        .filter(models.Post.channel_id.in_(subscribed_channel_ids))
+        .order_by(desc(models.Post.date))
+        .limit(3)
+        .all()
+    )
+
+    # Estructurar la respuesta
+    return {
+        "posts": [
+            {
+                "post_id": post.post_id,
+                "channel_name": post.channel_name
+            }
+            for post in recent_posts
+        ]
+    }
--- routes/posts_routes.py
+++ routes/posts_routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status, Query  # <-- Aquí agregamos Que
 from sqlalchemy.orm import Session
 import schemas
 from database import get_db
-from controllers.posts_controllers import get_posts_by_channel, create_post
+from controllers.posts_controllers import get_posts_by_channel, create_post, get_recent_user_posts
 
 post_router = APIRouter(prefix="/api/posts", tags=["Posts"])
 
@@ -17,3 +17,6 @@ def create_post_route(
 ):
     return create_post(post, post.user_id, db)
 
+@post_router.get("/user_recent_posts", response_model=dict)
+def get_recent_user_posts_route(user_id: int, db: Session = Depends(get_db)):
+    return get_recent_user_posts(user_id, db)
